b7884216839b6072c9eed4bc54c8635428372f47
Hamilton Santana <hamilton.santana@outlook.com>
2022-06-02 18:44:00 -0300

    add - new problem solution.
    - Sending first attempt for Problem2.

---
+++ Problem2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem2
+{
+    public class Problem2
+    {
+        static void Main()
+        {
+            var result = Incrementador(1.8);
+
+            Console.WriteLine(result);
+        }
+
+        public static dynamic Incrementador(dynamic valor)
+        {
+                return valor += 1;
+        }
+    }
+}
